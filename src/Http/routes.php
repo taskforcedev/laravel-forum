1d2dc10963f8c32cffac970caae07cc62bf005f8
PrivateSniper <privatesniper@gmail.com>
2015-06-06 19:47:23 +0100

    Comment on routes file.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,15 +1,19 @@
 <?php
 
 Route::group(['namespace' => 'Taskforcedev\LaravelForum\Http\Controllers'], function() {
-    Route::get('forum', ['as' => 'forum.index', 'uses' => 'ForumController@index']);
+    // Public Routes
+    Route::get('forum/{id}/post', ['as' => 'forum.write.post', 'uses' => 'ForumController@createPost']);
+    Route::get('forum/{id}',      ['as' => 'forum.view', 'uses' => 'ForumController@view']);
+    Route::get('forum',           ['as' => 'forum.index',  'uses' => 'ForumController@index']);
 
-
+    // Admin Routes.
     Route::group(['prefix' => 'admin'], function() {
         Route::get('forum',            ['as' => 'forum.admin.index', 'uses' => 'AdminController@index']);
         Route::get('forum/forums',     ['as' => 'forum.admin.forums', 'uses' => 'AdminController@forums']);
         Route::get('forum/categories', ['as' => 'forum.admin.categories', 'uses' => 'AdminController@categories']);
     });
 
+    // Api Routes.
     Route::group(['prefix' => 'api'], function() {
         Route::post('forum_category', ['as' => 'api.store.forum.category', 'uses' => 'ApiController@forumCategoryStore']);
         Route::post('forum',          ['as' => 'api.store.forum',          'uses' => 'ApiController@forumStore']);
