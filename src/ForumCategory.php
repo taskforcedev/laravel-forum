274366707aec6ca9d698d25ac56ea887a763a35b
PrivateSniper <privatesniper@gmail.com>
2015-06-06 16:26:35 +0100

    Add validation.

--- src/ForumCategory.php
+++ src/ForumCategory.php
@@ -7,4 +7,22 @@ class ForumCategory extends Model
     public $table = 'forum_categories';
 
     public $fillable = ['name'];
+
+    /**
+     * Is model data valid.
+     * @param array|object $data The data to validate.
+     * @return boolean
+     */
+    public static function valid($data)
+    {
+        $rules = [
+            'name' => ['required', 'min:3'],
+        ];
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->passes()) {
+            return true;
+        }
+        return false;
+    }
 }
