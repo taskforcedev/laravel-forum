60ed1d079b4672d91983745d0b5cb78f583e0fb3
PrivateSniper <privatesniper@gmail.com>
2015-06-06 16:25:45 +0100

    Add relation and helper methods.

--- src/Forum.php
+++ src/Forum.php
@@ -1,5 +1,6 @@
 <?php namespace Taskforcedev\LaravelForum;
 
+use \DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Forum extends Model
@@ -7,4 +8,46 @@ class Forum extends Model
     public $table = 'forums';
 
     public $fillable = ['name', 'description', 'category_id'];
+
+    /**
+     * Eloquent Relation
+     * @return mixed
+     */
+    public function category()
+    {
+        return $this->belongsTo('ForumCategory');
+    }
+
+    /**
+     * Get the number of threads inside the current forum.
+     * @return mixed
+     */
+    public function getThreadCount()
+    {
+        try {
+            $result = DB::select('SELECT COUNT(*) as count FROM `forum_posts` where forum_id = ?', [$this->id]);
+            return $result[0]->count;
+        } catch (\Exception $e) {
+            return 0;
+        }
+    }
+
+    /**
+     * Is model data valid.
+     * @param array|object $data The data to validate.
+     * @return boolean
+     */
+    public static function valid($data)
+    {
+        $rules = [
+            'name' => ['required', 'min:3'],
+            'category_id' => ['required', 'min:0', 'integer']
+        ];
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->passes()) {
+            return true;
+        }
+        return false;
+    }
 }
