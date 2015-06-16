94da9d42d861a02b0a818c84a01450c9811889a7
PrivateSniper <privatesniper@gmail.com>
2015-06-16 12:42:00 +0100

    Add modal type to config.

--- src/config/laravel-forum.php
+++ src/config/laravel-forum.php
@@ -4,4 +4,16 @@ return [
     'layout' => 'laravel-forum::layout.master',
 
     'sitename' => 'Laravel',
+
+    /*
+     * What type of modals (pop up forms) should the package use.
+     */
+    'modal' => [
+        /*
+         * Type of modal to use:
+         * Valid options: 'bootstrap', 'jquery',
+         * Currently Supported: 'bootstrap'
+         */
+        'type' => 'bootstrap',
+    ]
 ];
