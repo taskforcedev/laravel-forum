59cdeaa3546d44b1fac51e85cefb0bae91b59802
PrivateSniper <privatesniper@gmail.com>
2015-06-17 13:33:27 +0100

    Add link to post.

--- src/views/forum/forum.blade.php
+++ src/views/forum/forum.blade.php
@@ -5,5 +5,17 @@
 
     <?php
         $posts = $forum->posts;
+        $postCount = count($posts);
     ?>
+
+    @if ($postCount > 0)
+        <table class="table">
+        @foreach($posts as $post)
+            <?php $replies = $post->replies; $replyCount = count($post->replies); ?>
+            <tr><td><a href="{{ route('laravel-forum.view.post', ['id' => $forum->id, 'fid' $post->id]) }}">{{ $post->title }}</a></td>{{ $replyCount }}</tr>
+        @endforeach
+        </table>
+    @else
+        <p>There are currently no posts in this forum</p>
+    @endif
 @stop
