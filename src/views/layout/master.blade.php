a3dca47acb4f7c76a08bb017537e6a234372e3db
PrivateSniper <privatesniper@gmail.com>
2015-06-17 10:16:37 +0100

    Add bootstrap to master layout.

--- src/views/layout/master.blade.php
+++ src/views/layout/master.blade.php
@@ -3,6 +3,8 @@
 <head>
     <title>Laravel Forum</title>
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 @yield('content')
