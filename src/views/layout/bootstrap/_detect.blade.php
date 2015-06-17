5c58929abe6f8bc35bd0e107031dddd78ba8d1b5
PrivateSniper <privatesniper@gmail.com>
2015-06-17 10:08:22 +0100

    Alter bootstrap detection.

--- src/views/layout/bootstrap/_detect.blade.php
+++ src/views/layout/bootstrap/_detect.blade.php
@@ -1,14 +1,13 @@
 <?php
 /* This script detects if bootstrap is not defined on the page and if so adds it into the head. */
 ?><script>
-var bootstrap_enabled = (typeof $().modal == 'function');
-
-if (!bootstrap_enabled) {
-    $("head").prepend("<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'><\/script>");
-}
-
-<!-- BOOTSTRAP CDN FALLBACK CSS-->
 $(document).ready(function() {
+
+    var bootstrap_enabled = (typeof $().modal == 'function');
+    if (!bootstrap_enabled) {
+        $("head").prepend("<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'><\/script>");
+    }
+
     if( verifyStyle('form-inline') )
     {
     $("head").prepend("<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' type='text/css' media='screen'>");
