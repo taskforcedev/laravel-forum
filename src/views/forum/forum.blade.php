400291f715416db03edee47b208f4dd248f74339
PrivateSniper <privatesniper@gmail.com>
2015-06-17 13:14:09 +0100

    Display forum name

--- src/views/forum/forum.blade.php
+++ src/views/forum/forum.blade.php
@@ -1,5 +1,9 @@
 @extends($layout)
 
 @section('content')
-    <h1>{{ $sitename or 'Laravel' }} Forums</h1>
+    <h1>{{ $forum->name }}</h1>
+
+    <?php
+        $posts = $forum->posts;
+    ?>
 @stop
