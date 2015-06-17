1316ca22fb40eb2f2c12691822829dcf5dbd67fd
PrivateSniper <privatesniper@gmail.com>
2015-06-17 09:31:57 +0100

    Correct category api route.

--- src/views/admin/category/_create.blade.php
+++ src/views/admin/category/_create.blade.php
@@ -5,7 +5,7 @@ $fields = [
 $data = [
     'item' => 'Category',
     'fields' => $fields,
-    'url' => URL::route('api.store.forum.category')
+    'url' => URL::route('laravel-forum.api.store.forum.category')
 ];
 ?>
 @include('layout.bootstrap._createModal', $data)
