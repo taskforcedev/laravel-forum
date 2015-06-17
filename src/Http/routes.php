945c5c68c0f5b62c42b92b163cd4ca2e52b08238
PrivateSniper <privatesniper@gmail.com>
2015-06-17 09:03:52 +0100

    Update routes, longer names but now almost impossible to have any collision issues.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,22 +2,22 @@
 
 Route::group(['namespace' => 'Taskforcedev\LaravelForum\Http\Controllers'], function() {
     // Public Routes
-    Route::get('forum/{id}/post', [ 'as' => 'forum.write.post', 'uses' => 'ForumController@createPost' ]);
-    Route::get('forum/{id}',      [ 'as' => 'forum.view',       'uses' => 'ForumController@view'       ]);
-    Route::get('forum',           [ 'as' => 'forum.index',      'uses' => 'ForumController@index'      ]);
+    Route::get('forum/{id}/post', [ 'as' => 'laravel-forum.write.post', 'uses' => 'ForumController@createPost' ]);
+    Route::get('forum/{id}',      [ 'as' => 'laravel-forum.view',       'uses' => 'ForumController@view'       ]);
+    Route::get('forum',           [ 'as' => 'laravel-forum.index',      'uses' => 'ForumController@index'      ]);
 
     // Admin Routes.
     Route::group(['prefix' => 'admin'], function() {
-        Route::get('forum',            [ 'as' => 'forum.admin.index',      'uses' => 'AdminController@index'      ]);
-        Route::get('forum/forums',     [ 'as' => 'forum.admin.forums',     'uses' => 'AdminController@forums'     ]);
-        Route::get('forum/categories', [ 'as' => 'forum.admin.categories', 'uses' => 'AdminController@categories' ]);
+        Route::get('forum',            [ 'as' => 'laravel-forum.admin.index',      'uses' => 'AdminController@index'      ]);
+        Route::get('forum/forums',     [ 'as' => 'laravel-forum.admin.forums',     'uses' => 'AdminController@forums'     ]);
+        Route::get('forum/categories', [ 'as' => 'laravel-forum.admin.categories', 'uses' => 'AdminController@categories' ]);
     });
 
     // Api Routes.
     Route::group(['prefix' => 'api'], function() {
-        Route::post('forum_category', [ 'as' => 'api.store.forum.category', 'uses' => 'ApiController@forumCategoryStore' ]);
-        Route::post('forum',          [ 'as' => 'api.store.forum',          'uses' => 'ApiController@forumStore'         ]);
-        Route::post('forum_post',     [ 'as' => 'api.store.forum.post',     'uses' => 'ApiController@forumStore'         ]);
-        Route::post('forum_reply',    [ 'as' => 'api.store.forum.reply',    'uses' => 'ApiController@forumStore'         ]);
+        Route::post('forum_category', [ 'as' => 'laravel-forum.api.store.forum.category', 'uses' => 'ApiController@forumCategoryStore' ]);
+        Route::post('forum',          [ 'as' => 'laravel-forum.api.store.forum',          'uses' => 'ApiController@forumStore'         ]);
+        Route::post('forum_post',     [ 'as' => 'laravel-forum.api.store.forum.post',     'uses' => 'ApiController@forumStore'         ]);
+        Route::post('forum_reply',    [ 'as' => 'laravel-forum.api.store.forum.reply',    'uses' => 'ApiController@forumStore'         ]);
     });
 });
