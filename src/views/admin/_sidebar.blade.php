7033515467cebe4b83983de96d435a704c39e816
PrivateSniper <privatesniper@gmail.com>
2015-06-17 09:04:17 +0100

    Update routes.

--- src/views/admin/_sidebar.blade.php
+++ src/views/admin/_sidebar.blade.php
@@ -4,8 +4,8 @@
     </div>
     <div class="panel-body">
         <ul class="list-group">
-            <li class="list-group-item"><a href="{{ URL::route('forum.admin.forums') }}">Forums</a></li>
-            <li class="list-group-item"><a href="{{ URL::route('forum.admin.categories') }}">Categories</a></li>
+            <li class="list-group-item"><a href="{{ URL::route('laravel-forum.admin.forums') }}">Forums</a></li>
+            <li class="list-group-item"><a href="{{ URL::route('laravel-forum.admin.categories') }}">Categories</a></li>
         </ul>
     </div>
 </div>
