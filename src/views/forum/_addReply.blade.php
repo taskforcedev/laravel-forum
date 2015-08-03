7afe8fa39f0fddd49e84aef01ccd2228dfbc4cdd
PrivateSniper <privatesniper@gmail.com>
2015-08-03 16:21:21 +0100

    Add TinyMCE from CDN

--- src/views/forum/_addReply.blade.php
+++ src/views/forum/_addReply.blade.php
@@ -1,7 +1,29 @@
+<?php
+    if(isset($wysiwyg)) {
+        $wysiwygInclude = $wysiwyg['include'];
+
+        switch ($wysiwyg['name']) {
+            case 'tinymce':
+                $js = '//tinymce.cachefly.net/4.2/tinymce.min.js';
+                $css = null;
+                $init = "tinymce.init({selector:'textarea'});";
+        }
+    }
+?>
 @if (\Auth::check())
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h2>Reply</h2>
 
+        @if (isset($wysiwygInclude))
+            @if (isset($js))
+                <script src="{{ $js }}"></script>
+            @endif
+        @endif
         <textarea class="form-control"></textarea>
+        @if (isset($wysiwygInclude))
+            <script type="application/javascript">
+                <?=$init;?>
+            </script>
+        @endif
     </div>
 @endif
