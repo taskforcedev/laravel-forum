046e389d4dc82e7205a5192cc105afcfeec8dfa5
PrivateSniper <privatesniper@gmail.com>
2015-08-03 16:03:02 +0100

    Add form control class to reply textarea

--- src/views/forum/_addReply.blade.php
+++ src/views/forum/_addReply.blade.php
@@ -2,6 +2,6 @@
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h2>Reply</h2>
 
-        <textarea></textarea>
+        <textarea class="form-control"></textarea>
     </div>
 @endif
