f87b9a1735bce2c6a8e0219897e55f8a20260da0
PrivateSniper <privatesniper@gmail.com>
2015-08-03 16:10:58 +0100

    Add extra config options

--- src/config/laravel-forum.php
+++ src/config/laravel-forum.php
@@ -5,9 +5,7 @@ return [
 
     'sitename' => 'Laravel',
 
-    /*
-     * What type of modals (pop up forms) should the package use.
-     */
+    // What type of modals should the package use.
     'modal' => [
         /*
          * Type of modal to use:
@@ -15,5 +13,21 @@ return [
          * Currently Supported: 'bootstrap'
          */
         'type' => 'bootstrap',
+    ],
+
+    'wysiwyg' => [
+
+        /**
+         * Whether the package should include the WYSIWYG code (CSS/JS etc)
+         * if set to false then your layout will have to include it otherwise WYSIWYG won't be visible.
+         */
+        'include' => true,
+
+        /*
+         * Which WYSIWYG to use.  Only relevant if include is true.
+         * Valid Options: 'tinymce'
+         * Currently Supported: None - TODO
+         */
+        'name' => 'tinymce'
     ]
 ];
