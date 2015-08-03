f1b91af30ec25006f7a4465c37d4912bbf6b5234
PrivateSniper <privatesniper@gmail.com>
2015-08-03 15:46:22 +0100

    Add 404 Page

---
+++ src/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends($layout)
+
+@section('content')
+    <h1>404</h1>
+
+    The page you are looking for could not be found, why not go back to the <a href="{{ route('laravel-forum.index') }}">forum</a>?
+@stop
