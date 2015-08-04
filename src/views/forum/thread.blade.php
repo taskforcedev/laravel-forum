f5c7d062142eb3b9ffe27965262b4f2a727d381d
PrivateSniper <privatesniper@gmail.com>
2015-08-04 09:50:59 +0100

    Add forum replies and bootstrap layout

--- src/views/forum/thread.blade.php
+++ src/views/forum/thread.blade.php
@@ -1,9 +1,41 @@
 @extends($layout)
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
 
-    {!! nl2br(e($post->body)) !!}
+    <div class="row forum-post">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg2">
+            {{ $post->author->name }}
+        </div>
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+            <h1>{{ $post->title }}</h1>
+            {!! nl2br(e($post->body)) !!}
+        </div>
+    </div>
+
+    <?php
+        $replies = $post->replies;
+        if ($replies->isEmpty()) {
+            $replies = null;
+        }
+    ?>
+
+    @if (isset($replies))
+
+        @foreach ($replies as $reply)
+
+            <div class="row forum-reply">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg2">
+                    {{ $reply->author->name }}
+                </div>
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+                    <h1>{{ $reply->title }}</h1>
+                    {!! nl2br(e($reply->body)) !!}
+                </div>
+            </div>
+
+        @endforeach
+
+    @endif
 
     @include('laravel-forum::forum/_addReply')
 @stop
