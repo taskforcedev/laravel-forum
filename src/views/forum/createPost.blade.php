01ea306fa5907eac817979c5b65db8a04bca4c28
David Craig <privatesniper@gmail.com>
2015-08-11 15:59:11 +0100

    Add wysiwyg on createPost page.

--- src/views/forum/createPost.blade.php
+++ src/views/forum/createPost.blade.php
@@ -1,3 +1,16 @@
+<?php
+    if(isset($wysiwyg)) {
+        $wysiwygInclude = $wysiwyg['include'];
+
+        switch ($wysiwyg['name']) {
+            case 'tinymce':
+                $js = '//tinymce.cachefly.net/4.2/tinymce.min.js';
+                $css = null;
+                $init = "tinymce.init({selector:'textarea'});";
+        }
+    }
+?>
+
 @extends($layout)
 
 @section('content')
@@ -7,9 +20,21 @@
     <label class="label">Post Title</label>
     <input class="form-control" type="text" name="title" id="title" />
 
+    @if (isset($wysiwygInclude))
+        @if (isset($js))
+            <script src="{{ $js }}"></script>
+        @endif
+    @endif
+
     <label class="label">Body</label>
     <textarea class="form-control" name="body" id="body" rows="20">
     </textarea>
+
+    @if (isset($wysiwygInclude))
+        <script type="application/javascript">
+            <?=$init;?>
+        </script>
+    @endif
 
     <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}" />
     <input type="hidden" name="forum_id" id="forum_id" value="{{ $forum->id }}" />
