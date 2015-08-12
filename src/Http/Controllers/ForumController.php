fdacf9b06c2ede337ea314bd1fdbd1d1c2fac9e4
David Craig <privatesniper@gmail.com>
2015-08-12 17:54:23 +0100

    Remove extra check.

--- src/Http/Controllers/ForumController.php
+++ src/Http/Controllers/ForumController.php
@@ -49,10 +49,6 @@ class ForumController extends BaseController
         try {
             $post = ForumPost::with('replies')->where('id', $post_id)->firstOrFail();
 
-            if ($post->forum_id !== (int)$forum_id) {
-                throw new Exception("Forum ID's do not match, fid: {$forum_id}, post fid: {$post->forum_id}");
-            }
-
             $data = $this->buildData();
             $data['post'] = $post;
 
