5fa32e4c6fc1b26fa2396552b0e9a2b9610ea4cd
PrivateSniper <privatesniper@gmail.com>
2015-08-12 10:47:33 +0000

    Wrap content in a container

--- src/views/layout/master.blade.php
+++ src/views/layout/master.blade.php
@@ -7,7 +7,9 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-@yield('breadcrumbs')
-@yield('content')
+    <div class="container">
+        @yield('breadcrumbs')
+        @yield('content')
+    </div>
 </body>
 </html>
