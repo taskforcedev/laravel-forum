c248ec8bcf87af0f23c11a1c75b0375fb109fe71
PrivateSniper <privatesniper@gmail.com>
2015-08-14 12:02:02 +0000

    Align columns

--- src/views/admin/index.blade.php
+++ src/views/admin/index.blade.php
@@ -1,10 +1,10 @@
 @extends($layout)
 
 @section('content')
-    <h1>Forum Administration</h1>
-
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+
+            <h1>Laravel-Forum: Administration</h1>
 
             <p>Please use the navigation on the right to manage forums and categories.</p>
 
