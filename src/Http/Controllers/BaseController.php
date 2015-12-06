104c30a5bb7f242fc9e5d20d8b1bdb6e927aee2d
PrivateSniper <privatesniper@gmail.com>
2015-12-06 19:29:43 +0000

    use laravel support controller, remove redundant code.

--- src/Http/Controllers/BaseController.php
+++ src/Http/Controllers/BaseController.php
@@ -2,7 +2,7 @@
 
 use \Auth;
 use \Schema;
-use Illuminate\Routing\Controller;
+use Taskforcedev\LaravelSupport\Http\Controllers\Controller;
 use Taskforcedev\LaravelForum\ForumCategory;
 use Taskforcedev\LaravelForum\Helpers\UserHelper;
 use Taskforcedev\LaravelForum\Database\Migrator;
@@ -25,50 +25,12 @@ class BaseController extends Controller
     }
 
     /**
-     * Returns the user object (or guest).
-     * @return object
-     */
-    public function getUser()
-    {
-        return (Auth::check() ? \Auth::user() : $this->guest());
-    }
-
-    /**
-     * Gets the layout from configuration.
-     * @return mixed
-     */
-    public function getLayout()
-    {
-        return config('laravel-forum.layout');
-    }
-
-    /**
      * Gets the modal type preference from config.
      * @return mixed
      */
     public function getModalType()
     {
         return config('laravel-forum.modal.type');
-    }
-
-    /**
-     * Determine if the user can administrate the forums
-     * @return boolean
-     */
-    protected function canAdministrate()
-    {
-        $user = $this->getUser();
-
-        if ($user->name == 'Guest' && $user->email == 'guest@example.com') {
-            return false;
-        }
-
-        if (method_exists($user, 'can')) {
-            return $user->can('forum-administrate');
-        }
-
-        // If no method of authorizing return false;
-        return false;
     }
 
     /**
@@ -89,34 +51,6 @@ class BaseController extends Controller
 
         // If no method of authorizing return false;
         return false;
-    }
-
-    /**
-     * Retrieve a user model or object for the Guest user.
-     * @return object
-     */
-    public function guest()
-    {
-        /* Get the namespace */
-        $model = $this->getUserModel();
-        if ($model) {
-            $guest = new $model();
-            $guest->name = 'Guest';
-            $guest->email = 'guest@example.com';
-        } else {
-            $guest = (object)['name' => 'Guest', 'email' => 'guest@example.com'];
-        }
-        return $guest;
-    }
-
-    /**
-     * Attempt to retrieve the user model class name (namespaced).
-     * @return boolean|string
-     */
-    public function getUserModel()
-    {
-        $helper = new UserHelper();
-        return $helper->getUserModel();
     }
 
     /**
