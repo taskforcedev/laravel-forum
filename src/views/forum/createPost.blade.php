d05514d4e473d7cf8f822b297d715318b3c146f6
PrivateSniper <privatesniper@gmail.com>
2016-08-24 15:00:13 +0100

    Temporarily remove wysiwyg.

--- src/views/forum/createPost.blade.php
+++ src/views/forum/createPost.blade.php
@@ -1,16 +1,3 @@
-<?php
-    if(isset($wysiwyg)) {
-        $wysiwygInclude = $wysiwyg['include'];
-
-        switch ($wysiwyg['name']) {
-            case 'tinymce':
-                $js = '//tinymce.cachefly.net/4.2/tinymce.min.js';
-                $css = null;
-                $init = "tinymce.init({selector:'textarea'});";
-        }
-    }
-?>
-
 @extends($layout)
 
 @section('content')
@@ -20,26 +7,12 @@
     <label>Post Title</label>
     <input class="form-control" type="text" name="title" id="title" />
 
-    @if (isset($wysiwygInclude))
-        @if (isset($js))
-            <script src="{{ $js }}"></script>
-        @endif
-    @endif
-
     <label>Body</label>
     <textarea class="form-control" name="body" id="body" rows="20">
     </textarea>
 
-    @if (isset($wysiwygInclude))
-        <script type="application/javascript">
-            <?=$init;?>
-        </script>
-    @endif
-
     <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}" />
     <input type="hidden" name="forum_id" id="forum_id" value="{{ $forum->id }}" />
-
     <input type="submit" class="btn btn-success" />
-
 </form>
 @stop
