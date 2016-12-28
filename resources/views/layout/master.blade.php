63e2bc27dfb4039bb7f1e3bd4b4e7dce310c49b3
PrivateSniper <privatesniper@gmail.com>
2016-12-28 15:45:30 +0000

    Yield scripts, head, styles.

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,24 +5,14 @@
     <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    @yield('head')
+    @yield('styles')
 </head>
 <body>
     <div class="container">
         @yield('breadcrumbs')
         @yield('content')
     </div>
-    @if (isset($wysiwyg))
-        @if ($wysiwyg['include'] === true)
-            @if ($wysiwyg['name'] === 'tinymce')
-                <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-            @endif
-        @endif
-        @if ($wysiwyg['name'] === 'tinymce')
-            <script>
-                tinymce.init({ selector:'.wysiwyg' });
-            </script>
-        @endif
-    @endif
     @yield('scripts')
 </body>
 </html>
