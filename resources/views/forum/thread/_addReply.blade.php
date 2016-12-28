05921dc0846ee3abe9e42773f4af15ceb1fdfd58
PrivateSniper <privatesniper@gmail.com>
2016-12-28 15:31:18 +0000

    Add wysiwyg class to add reply textarea.

--- resources/views/forum/thread/_addReply.blade.php
+++ resources/views/forum/thread/_addReply.blade.php
@@ -3,7 +3,7 @@
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h2>Reply</h2>
 
-        <textarea name="body" class="form-control"></textarea>
+        <textarea name="body" class="form-control wysiwyg"></textarea>
 
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
         <input type="hidden" name="forum_id" value="{{ $forum->id }}" />
