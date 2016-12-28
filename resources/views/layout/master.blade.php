7a7b943855e8f8a90bf7c18c4cc3bedfdddce2a3
PrivateSniper <privatesniper@gmail.com>
2016-12-28 14:28:23 +0000

    Add wysiwyg scripts for tinymce.

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,5 +11,18 @@
         @yield('breadcrumbs')
         @yield('content')
     </div>
+    @if (isset($wysiwyg))
+        @if ($wysiwyg['include'] === true)
+            @if ($wysiwyg['name'] === 'tinymce')
+                <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+            @endif
+        @endif
+        @if ($wysiwyg['name'] === 'tinymce')
+            <script>
+                tinymce.init({ selector:'.wysiwyg' });
+            </script>
+        @endif
+    @endif
+    @yield('scripts')
 </body>
 </html>
