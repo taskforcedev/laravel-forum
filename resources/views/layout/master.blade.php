5948baece9e0c2ab68951bae74c660d56f03d4bc
PrivateSniper <privatesniper@gmail.com>
2016-12-28 10:24:45 +0000

    Update bootstrap and jquery version numbers.

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <title>Laravel Forum</title>
-    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
